6ff2a8b7abfb3d7084db2e2238c01a5d9bd58fe8
Nasibb <nasibgi@code.edu.az>
2023-10-16 23:52:39 +0400

    firstTask

---
+++ Task141023/Program.cs
@@ -0,0 +1,11 @@
+﻿#region Rəqəmlərin yerin dəyiş.
+
+int n = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
+m = m + n;
+n = m - n;
+m = m - n;
+Console.WriteLine(n);
+Console.WriteLine(m);
+
+#endregion
